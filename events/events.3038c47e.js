b4350c9819aea5a2a1489bfdcc10ea37c3603a5d
Vivdyuk <84183046+Vivdyuk@users.noreply.github.com>
2022-12-02 14:01:48 +0000

    Deploying to gh-pages from @ Vivdyuk/bootcamp_mocks@8d172b41138d27ff5c9b7857d5534bf6e0a941b0 🚀

---
+++ events/events.3038c47e.js
@@ -0,0 +1,2 @@
+document.querySelector(".button").addEventListener("click",(function(){}));
+//# sourceMappingURL=events.3038c47e.js.map
---
+++ events/events.fb5815cc.js
@@ -0,0 +1,2 @@
+document.querySelector(".button").addEventListener("click",(()=>{}));
+//# sourceMappingURL=events.fb5815cc.js.map
